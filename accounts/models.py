08a8c6169e4dad504f0fcc0eae6435d2f7aacf4f
Geoffy1 <geojogu@gmail.com>
2025-09-27 20:46:45 +0300

    fixing docker

--- accounts/models.py
+++ accounts/models.py
@@ -11,4 +11,3 @@ class User(AbstractUser):
         (ROLE_RECRUITER, "Recruiter"),
     ]
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default=ROLE_USER)
-
--- jobboard/settings.py
+++ jobboard/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     "accounts",
     "jobs",
     "applications",
+    'django.contrib.postgres',
 ]
 
 MIDDLEWARE = [
